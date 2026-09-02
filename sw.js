4ae19ecd2c2fdc06501aff916fb795506ab26783
logic050122-cmyk <logic050122@gmail.com>
2026-09-02 09:52:56 +0000

    Add axis-lock and free movement modes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dual-axis-dodge-v2';
+const CACHE = 'dual-axis-dodge-v3';
 const CORE = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
